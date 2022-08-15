a19b7569f1c16af16b3a162773fdbb06046afe58
Jo <88668777+jonars99@users.noreply.github.com>
2022-08-15 14:43:26 +0100

    set up states and input handlers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ location, setLocation ] = useState({
+    latitude: "",
+    longitude: "",
+  });
+
+  const handleLat = (e) => {
+    console.log(e.target.name, e.target.value);
+    setLocation({...location,
+      latitude: e.target.value,
+    })
+  }
+  
+  const handleLong = (e) => {
+    console.log(e.target.name, e.target.value);
+    setLocation({...location,
+    longitude: e.target.value,
+  })
+  }
+
   return (
     <div className="App App-header">
       <h2>Weather Checker</h2>
@@ -8,10 +28,10 @@ function App() {
 
       <div className="input-wrapper">
         <form>
-          <label for="latitude">Latitude</label>
-          <input id="latitude"></input>
-          <label for="longitude">Longitude</label>
-          <input id="longitude"></input>
+          <label htmlFor="latitude">Latitude</label>
+          <input id="latitude" name="latitude" onChange={handleLat} value={location.latitude}></input>
+          <label htmlFor="longitude">Longitude</label>
+          <input id="longitude" name="longitude" onChange={handleLong} value={location.longitude}></input>
           <button>Get Weather Details</button>
         </form>
       </div>
