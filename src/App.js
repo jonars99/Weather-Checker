02f1b531c661ebc398011759f0c3c936448275cf
Jo <88668777+jonars99@users.noreply.github.com>
2022-08-15 14:27:13 +0100

    set up basic layout/text

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+      <h2>Weather Checker</h2>
+      <p>Please enter your latitude and longitude to check the weather</p>
+
+      <div className="input-wrapper">
+        <form>
+          <label for="latitude">Latitude</label>
+          <input id="latitude"></input>
+          <label for="longitude">Longitude</label>
+          <input id="longitude"></input>
+          <button>Get Weather Details</button>
+        </form>
+      </div>
+
     </div>
   );
 }
